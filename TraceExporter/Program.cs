8dce9d9341da7479787c1f801a9bc5155f3d8991
heathluo <heath.luo@msn.com>
2021-12-21 18:47:29 +0800

    support using 'Process ID' in Instruments to query

--- TraceExporter/Program.cs
+++ TraceExporter/Program.cs
@@ -22,10 +22,18 @@ namespace TraceExporter
             }
 
             //var memNodes = doc.SelectNodes("/trace-query-result/node/row/process[@ref=\"5391\"]/../size-in-bytes[1]/@fmt");
+            var targetProcessNode = doc.SelectSingleNode($"/trace-query-result/node/row/process/pid[@fmt={args[1]}]/..");
+            if(targetProcessNode == null)
+            {
+                Console.WriteLine($"Can't find the specified process {args[1]}");
+                return;
+            }
+
+            string processID = targetProcessNode.Attributes["id"].Value;
 
             StreamWriter file = new StreamWriter("result.csv");
             // select the specified process nodes
-            var processNodes = doc.SelectNodes($"/trace-query-result/node/row/process[@ref={args[1]}]/..");
+            var processNodes = doc.SelectNodes($"/trace-query-result/node/row/process[@ref={processID}]/..");
             foreach(XmlNode node in processNodes)
             {
                 string time;
@@ -34,7 +42,7 @@ namespace TraceExporter
                     time = node["start-time"].GetAttribute("fmt");
                 else
                     mapIdTime.TryGetValue(refTime, out time);
-                if(!string.IsNullOrEmpty(time))
+                if (!string.IsNullOrEmpty(time))
                 {
                     string fmtMem = node["size-in-bytes"].GetAttribute("fmt");
                     if (!string.IsNullOrEmpty(fmtMem))
